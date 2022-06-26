a1047af4545b55dbaadda0987e9009b2214907fd
Ernest Mihasenko <ernestmihasenko@MacBook-Pro-3.local>
2022-06-26 23:25:36 +0300

    Implemented adding students with SwiftUI

---
+++ Lesson31HomeWork/Lesson31HomeWork/Lesson31HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson31HomeWorkApp.swift
+//  Lesson31HomeWork
+//
+//  Created by Ernest Mihasenko on 26.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson31HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
